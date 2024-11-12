78d8c578fc3a6ab1a83aa987124c43b520765b78
drordc <153188312+Blackhat364@users.noreply.github.com>
2024-11-12 15:08:41 +0200

    Update hash_library.h
    more good hash table

--- database/database_api/hash_library.h
+++ database/database_api/hash_library.h
@@ -8,6 +8,20 @@
 #define HASH_TABLE_EMAIL 2
 #define HASH_TABLE_PHON 3
 
+//flags
+#define FLAG_BIT    0b11111111000000000000000000000000
+#define CHANGES_BIT 0b00000000111111110000000000000000
+#define KEY_BIT     0b00000000000000001111111111111111
+#define OCCUIPIED_FLAG 0b00000001000000000000000000000000
+
+#define NUM_OF_SHIFTING_FLAG_BIT 24
+#define NUM_OF_SHIFTING_CHANGES_BIT 16
+
+
+
+#define MAX_MOVE 255
+#define MAX_SIZE_HASH_KEY_SIZE 65535
+
 
 
 
@@ -23,4 +37,8 @@ int pot_in_hash_table(Tables* database, Flag level,...);
 int search_in_hash_table(Tables* database, Flag table, char* string);
 
 int print_hash_tabel(Tables* database, Flag table);
+
+
+int SHIFTING_RIGHT(int * pointer,int num_of_shifting);
+
 #endif	
